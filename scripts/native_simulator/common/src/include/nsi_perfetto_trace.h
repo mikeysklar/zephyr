8801b409ec554cfd217c159c00f91280ea1331db
Scott Shawcroft <scott@tannewt.org>
2026-02-03 10:50:06 -0800

    Add gpio emulation and perfetto trace input

---
+++ scripts/native_simulator/common/src/include/nsi_perfetto_trace.h
@@ -0,0 +1,28 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef NSI_PERFETTO_TRACE_H
+#define NSI_PERFETTO_TRACE_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void nsi_perfetto_trace_set_path(const char *path);
+bool nsi_perfetto_trace_enabled(void);
+bool nsi_perfetto_trace_get_counter_int(const char *track_name, uint64_t timestamp_ns,
+					int64_t *value);
+bool nsi_perfetto_trace_get_counter_double(const char *track_name, uint64_t timestamp_ns,
+					   double *value);
+bool nsi_perfetto_trace_get_next_counter_time(const char *track_name, uint64_t timestamp_ns,
+					   uint64_t *next_timestamp_ns);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NSI_PERFETTO_TRACE_H */
